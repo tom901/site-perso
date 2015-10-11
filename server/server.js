c2d0167ea7749c96cd2d3a1f365dc6245a44c285
tom901 <thomas.paraiso@me.com>
2015-10-11 10:54:08 +0200

    Commit du fichier server

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
+//to load the index.html
 app.use(express.static(path.join(__dirname, '..', 'app')));
 
 // development only
