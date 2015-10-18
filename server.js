443dc7702705cf1609949e9c62b04536537cd044
tom901 <thomas.paraiso@me.com>
2015-10-18 23:11:01 +0200

    Quelques modifications sur le serveur, ajout d'une route project et d'un fichier project

--- server.js
+++ server.js
@@ -1,23 +1,35 @@
-var express = require('express')
-  , http = require('http')
-  , path = require('path')
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var fs = require('fs');
 
 var app = express();
 
 app.set('port', process.env.PORT || 9000);
-app.use(express.favicon());
+// app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 //to load the index.html
-app.use(express.static(path.join(__dirname, '/', 'app')));
+app.use('/', express.static(path.join(__dirname, 'app')));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
+app.get('/api/projects', function(req, res) {
+	console.log('entré dans api');
+  fs.readFile('projects.json', function(err, data) {
+    res.setHeader('Cache-Control', 'no-cache');
+	console.log('avant de parse');
 
-http.createServer(app).listen(app.get('port'), function(){
+    res.json(JSON.parse(data));
+  });
+});
+
+app.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
